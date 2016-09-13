1384c628d36a530a2d6fce335a82b29065119bf4
Toni Pizà <tpiza@habitissimo.com>
2016-09-13 17:05:41 +0200

    Added connect_timeout on repmgr config

--- bin/repmgr_configure.sh
+++ bin/repmgr_configure.sh
@@ -10,7 +10,7 @@ echo "127.0.0.1 $CLUSTER_NODE_NETWORK_NAME" >> /etc/hosts
 echo "cluster=$CLUSTER_NAME
 node=$NODE_ID
 node_name=$NODE_NAME
-conninfo='user=$REPLICATION_USER password=$REPLICATION_PASSWORD host=$CLUSTER_NODE_NETWORK_NAME dbname=$REPLICATION_DB port=$REPLICATION_PRIMARY_PORT'
+conninfo='user=$REPLICATION_USER password=$REPLICATION_PASSWORD host=$CLUSTER_NODE_NETWORK_NAME dbname=$REPLICATION_DB port=$REPLICATION_PRIMARY_PORT connect_timeout=$CONNECT_TIMEOUT'
 failover=automatic
 promote_command='PGPASSWORD=$REPLICATION_PASSWORD repmgr standby promote --log-level DEBUG --verbose'
 follow_command='PGPASSWORD=$REPLICATION_PASSWORD repmgr standby follow -W --log-level DEBUG --verbose'
