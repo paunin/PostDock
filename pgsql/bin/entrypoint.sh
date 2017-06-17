85cb93c1a15a2702cb4cc454fee699ee4bd2efb2
paunin <d.m.paunin@gmail.com>
2017-06-17 22:44:30 +0700

    Fix exit procedure

--- pgsql/bin/entrypoint.sh
+++ pgsql/bin/entrypoint.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 set -e
+
 echo ">>> Setting up STOP handlers..."
 for f in TERM SIGTERM QUIT SIGQUIT INT SIGINT KILL SIGKILL; do
     trap "system_stop $f" "$f"
 done
 
 echo '>>> STARTING POSTGRES...'
-/usr/local/bin/cluster/postgres/entrypoint.sh & wait ${!} || echo ">>> Foreground processes returned non zero code - $?"
+/usr/local/bin/cluster/postgres/entrypoint.sh & wait ${!} || echo ">>> Foreground processes returned code: '$?'"
 
-if [ -f "$STOPPING_LOCK_FILE" ]; then
-    ROSTGRES_STOPPING_PID=`head -1 $STOPPING_LOCK_FILE`
-    echo ">>> Waiting for postgres stopping process[PID=$ROSTGRES_STOPPING_PID]"
-    while kill -0 "$ROSTGRES_STOPPING_PID" 2> /dev/null; do sleep 1; done;
-    echo "============ Good bye ============"
-fi
+system_exit
