3b91f8d78a52f723e603b6fd5a5e4da417282e94
Dmitriy Paunin <d.m.paunin@gmail.com>
2017-07-17 11:04:47 +0700

    Fix healthcheck for PGpool

--- pgpool/bin/has_write_node.sh
+++ pgpool/bin/has_write_node.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PRIMARY_COUNT=$(PGCONNECT_TIMEOUT=$CHECK_PGCONNECT_TIMEOUT PGPASSWORD=$CHECK_PASSWORD psql -U $CHECK_USER -h localhost template1 -c 'show pool_nodes' | grep primary | wc -l)
+PRIMARY_COUNT=$(PGCONNECT_TIMEOUT=$CHECK_PGCONNECT_TIMEOUT PGPASSWORD=$CHECK_PASSWORD psql -U $CHECK_USER -h 127.0.0.1 template1 -c 'show pool_nodes' | grep primary | wc -l)
 
 if [[ "$PRIMARY_COUNT" == "0" ]]; then
     exit 1
