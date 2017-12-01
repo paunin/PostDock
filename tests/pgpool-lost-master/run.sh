c2cec3c8f9bec572089d404e142f5fe03b240ab0
Dmitriy Paunin <d.m.paunin@gmail.com>
2017-12-01 14:19:55 +0700

    fix (#99)

--- tests/pgpool-lost-master/run.sh
+++ tests/pgpool-lost-master/run.sh
@@ -10,15 +10,15 @@ docker-compose exec -T pgslave3 wait_local_postgres
 sleep 20
 docker-compose kill pgmaster
 
+# Generate some load
+for i in `seq 1 50`; do
+  docker-compose exec -T pgpool bash -c 'PGCONNECT_TIMEOUT=10 PGPASSWORD=$CHECK_PASSWORD psql -U $CHECK_USER -h 127.0.0.1 template1 -c "SELECT 1;"' ;
+done
+
 #Calculate delay for node to be marked as down
 # health_check_period + health_check_max_retries*(health_check_timeout + health_check_retry_delay)
 # 30 + 5 * (10 + 5) = 105 (+ some time)
-
-sleep 125
-# Generate some load
-for i in `seq 1 3`; do
-  docker-compose exec -T pgpool bash -c 'PGCONNECT_TIMEOUT=10 PGPASSWORD=$CHECK_PASSWORD psql -U $CHECK_USER -h 127.0.0.1 template1 -c "SELECT 1;"' ;
-done
+sleep 180
 
 MASTER_IS_OUT=`docker-compose exec -T pgpool bash -c 'PGCONNECT_TIMEOUT=10 PGPASSWORD=$CHECK_PASSWORD psql -U $CHECK_USER -h 127.0.0.1 template1 -c "show pool_nodes"' | grep pgmaster | awk -F"|" '{print $4}' | grep 'down\|3' | wc -l | tr -d ' '`
 if [[ "$MASTER_IS_OUT" != "1" ]]; then
--- tests/pgpool-lost-slave/run.sh
+++ tests/pgpool-lost-slave/run.sh
@@ -9,11 +9,17 @@ docker-compose exec -T pgslave3 wait_local_postgres
 sleep 20
 docker-compose kill pgslave3
 
+# Generate some load
+for i in `seq 1 50`; do
+  docker-compose exec -T pgpool bash -c 'PGCONNECT_TIMEOUT=10 PGPASSWORD=$CHECK_PASSWORD psql -U $CHECK_USER -h 127.0.0.1 template1 -c "SELECT 1;"' ;
+done
+
+
 #Calculate delay for node to be marked as down
 # health_check_period + health_check_max_retries*(health_check_timeout + health_check_retry_delay)
 # 30 + 5 * (10 + 5) = 105 (+ some time)
 
-sleep 135
+sleep 180
 
 NODE_IS_OUT=`docker-compose exec -T pgpool bash -c 'PGCONNECT_TIMEOUT=10 PGPASSWORD=$CHECK_PASSWORD psql -U $CHECK_USER -h 127.0.0.1 template1 -c "show pool_nodes"' | grep pgslave3 | awk -F"|" '{print $4}' | grep 'down\|3' | wc -l | tr -d ' '`
 if [[ "$NODE_IS_OUT" != "1" ]]; then
