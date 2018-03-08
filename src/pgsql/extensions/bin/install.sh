b876c73ab4e62b066fb978b0c6beed08cdd2a2d4
Dmitry Paunin <dmitry.paunin@lazada.com>
2018-03-08 11:31:54 +0700

    pgl_ddl_deploy

--- src/pgsql/extensions/bin/install.sh
+++ src/pgsql/extensions/bin/install.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 set -x
+set -e
 
 EXTENSIONS="$1"
 apt-get update
 echo "> Will install extensions: $EXTENSIONS"
-
+dir=$(pwd)
 for EXTENSION in $EXTENSIONS;
 do
     echo ">>> Installing now $EXTENSION"
     source /extensions_installer/extensions/$EXTENSION/install.sh
+    cd $dir
 done
 apt-get clean
