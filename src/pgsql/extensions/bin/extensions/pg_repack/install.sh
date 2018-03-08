679e0e16831f0882e3e4115d036231806f53ffe3
Dmitriy Paunin <d.m.paunin@gmail.com>
2018-03-08 20:58:07 +0800

    add pg_repack (#135)

---
+++ src/pgsql/extensions/bin/extensions/pg_repack/install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+apt-get install -y make unzip gcc libssl-dev zlib1g-dev
+wget -q -O pg_repack.zip "https://api.pgxn.org/dist/pg_repack/1.4.2/pg_repack-1.4.2.zip"
+unzip pg_repack.zip && rm pg_repack.zip
+cd pg_repack-*
+make && make install
+cd ..
+rm -rf pg_repack-*
+apt-get remove --auto-remove -y make unzip gcc libssl-dev zlib1g-dev
