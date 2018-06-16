982fbd1af1e80be2107a8d46c3f5a530cc392dc9
Dmitry Paunin <dmitry.paunin@lazada.com>
2018-06-16 15:44:51 +0700

    Fix version of pgosm

--- src/pgsql/extensions/bin/extensions/pgosm/install.sh
+++ src/pgsql/extensions/bin/extensions/pgosm/install.sh
@@ -2,9 +2,9 @@
 
 apt-get install -y make build-essential
 cd /tmp && \
-    wget -qO-  https://github.com/openstreetmap/openstreetmap-website/archive/master.tar.gz | tar xz
+    wget -qO-  https://github.com/openstreetmap/openstreetmap-website/archive/e4b94f4e5ef195dd5743ab3a510caecb7525a3b7.tar.gz | tar xz
 
-cd /tmp/openstreetmap-website-master/db/functions 
+cd /tmp/openstreetmap-website-*/db/functions 
 make libpgosm.so 
 cp libpgosm.so /usr/lib/postgresql/$PG_MAJOR/lib
 
