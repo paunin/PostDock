7fce3ef44624a43375cd03d494f1c27bf98312bd
Dmitriy Paunin <d.m.paunin@gmail.com>
2018-07-10 22:01:03 +0300

    ISSUE-97 (#161)
    * ISSUE-97
    
    * Issue-97

--- make/docker-compose/make.sh
+++ make/docker-compose/make.sh
@@ -3,7 +3,7 @@ echo ">>> Making docker-compose"
 FILE_FROM='./src/includes/docker-compose/docker-compose.yml'
 for POSTGRES_VERSION in 9.5 9.6 10; do
     for POSTGRES_EXTENDED in '' '1'; do
-        for REPMGR_VERSION in 3.2; do
+        for REPMGR_VERSION in 3.2 4.0; do
             for PGPOOL_VERSION in 3.3 3.6 3.7; do
                 for BARMAN_VERSION in 2.3 2.4; do
                     # For postgres 9.5 we can use client 9.6
@@ -20,7 +20,7 @@ for POSTGRES_VERSION in 9.5 9.6 10; do
                     fi
                     VALS="POSTGRES_EXTENDED=$POSTGRES_EXTENDED FILE_EXTENDED_SUFFIX=$FILE_EXTENDED_SUFFIX POSTGRES_VERSION=$POSTGRES_VERSION PGPOOL_VERSION=$PGPOOL_VERSION BARMAN_VERSION=$BARMAN_VERSION REPMGR_VERSION=$REPMGR_VERSION PGPOOL_POSTGRES_CLIENT_VERSION=$PGPOOL_POSTGRES_CLIENT_VERSION BARMAN_POSTGRES_CLIENT_VERSION=$BARMAN_POSTGRES_CLIENT_VERSION"
                     
-                    FILE_TO="./docker-compose/postgres${FILE_EXTENDED_SUFFIX}-${POSTGRES_VERSION}_pgpool-${PGPOOL_VERSION}_barman-${BARMAN_VERSION}.yml"
+                    FILE_TO="./docker-compose/postgres${FILE_EXTENDED_SUFFIX}-${POSTGRES_VERSION}_repmgr-${REPMGR_VERSION}_pgpool-${PGPOOL_VERSION}_barman-${BARMAN_VERSION}.yml"
                     template $FILE_FROM $FILE_TO $VALS
                 done
             done
