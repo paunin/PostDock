43522fdc2b077fa2aaaa0c03bde77d65d7a04ecb
rotulet <rotulet@gmail.com>
2018-10-17 09:39:46 +0200

    add postgis scripts to postgis extension installation and add pgrouting extension

--- src/pgsql/extensions/bin/extensions/postgis/install.sh
+++ src/pgsql/extensions/bin/extensions/postgis/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 POSTGIS_VERSION=2.4
-apt-get install -y postgresql-$PG_MAJOR-postgis-$POSTGIS_VERSION
+apt-get install -y postgresql-$PG_MAJOR-postgis-$POSTGIS_VERSION postgresql-$PG_MAJOR-postgis-$POSTGIS_VERSION-scripts
