24ed9dfd710eb847099cc318bd69f053b9a4808d
weasellin <weasellin@gmail.com>
2019-07-03 16:49:50 +0900

    Upgrade the extensions version for Postgres-11
    - Upgrade `pg_repack` from 1.4.3 to 1.4.4
      - To include the [fixing of the build failure](https://github.com/reorg/pg_repack/pull/191).
    - Upgrade PostGIS from 2.4 to 2.5
      - The 2.4 release (currently 2.4.7) only supports PostgreSQL 9.3-10.
      - The 2.5 release (currently 2.5.2) supports PostgreSQL 9.3-11.
      - Referring to [this release note](https://postgis.net/2019/03/11/postgis-patches/).

--- src/pgsql/extensions/bin/extensions/pg_repack/install.sh
+++ src/pgsql/extensions/bin/extensions/pg_repack/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 apt-get install -y make unzip gcc libssl-dev zlib1g-dev
-wget -q -O pg_repack.zip "https://api.pgxn.org/dist/pg_repack/1.4.3/pg_repack-1.4.3.zip"
+wget -q -O pg_repack.zip "https://api.pgxn.org/dist/pg_repack/1.4.4/pg_repack-1.4.4.zip"
 unzip pg_repack.zip && rm pg_repack.zip
 cd pg_repack-*
 make && make install
--- src/pgsql/extensions/bin/extensions/postgis/install.sh
+++ src/pgsql/extensions/bin/extensions/postgis/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-POSTGIS_VERSION=2.4
+POSTGIS_VERSION=2.5
 apt-get install -y postgresql-$PG_MAJOR-postgis-$POSTGIS_VERSION
