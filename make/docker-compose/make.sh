97c421f34973e8cc93cd1b407006d99014b6c617
weasellin <weasellin@gmail.com>
2019-07-08 09:45:13 +0900

    Exclude Combinations PostgreSQL-11 + repmgr-3.2
    Since they are not compatible.
    https://repmgr.org/docs/current/install-requirements.html#INSTALL-COMPATIBILITY-MATRIX

--- make/docker-compose/make.sh
+++ make/docker-compose/make.sh
@@ -6,6 +6,10 @@ for POSTGRES_VERSION in 9.5 9.6 10 11; do
         for REPMGR_VERSION in 3.2 4.0; do
             for PGPOOL_VERSION in 3.3 3.6 3.7; do
                 for BARMAN_VERSION in 2.3 2.4; do
+                    # postgres 11 and repmgr 3.2 are not compatible
+                    if [[ "$POSTGRES_VERSION" = "11" ]] && [[ "$REPMGR_VERSION" = "3.2" ]];then
+                        continue
+                    fi
                     # For postgres 9.5 we can use client 9.6
                     if [[ "$POSTGRES_VERSION" = "9.5" ]];then
                         PGPOOL_POSTGRES_CLIENT_VERSION="9.6"
--- src/includes/dockerfile/Postgres-extended-11-Repmgr-3.2.part.Dockerfile
+++
@@ -1,1 +0,0 @@
-Postgres-extended-9.5-11-Repmgr-3.2-4.0.part.Dockerfile
