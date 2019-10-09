f5de197eef53f376937156056393d650f3a9629b
Denis Miorandi <denis.miorandi@gmail.com>
2019-10-09 12:33:38 +0200

    Clean sockets files that prevent restart of pgPool and generate errors like: (#245)
    FATAL:  failed to bind a socket: "/var/run/postgresql/.s.PGSQL.5432"
    DETAIL:  bind socket failed with error: "Address already in use"

--- src/pgpool/bin/entrypoint.sh
+++ src/pgpool/bin/entrypoint.sh
@@ -6,6 +6,8 @@ export PCP_FILE='/usr/local/etc/pcp.conf'
 export HBA_FILE='/usr/local/etc/pool_hba.conf'
 export POOL_PASSWD_FILE='/usr/local/etc/pool_passwd'
 
+echo '>>> CLEANING environment (if required)...'
+rm -f /var/run/postgresql/.s.PGSQL*
 
 echo '>>> STARTING SSH (if required)...'
 sshd_start
