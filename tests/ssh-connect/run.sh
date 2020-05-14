77e1f435a1d121a4b3100593efa4f59499f5ed23
stegben <stegben.benjamin@gmail.com>
2020-05-14 17:05:27 +0800

    add test case for ssh connection under restart pgslave

--- tests/ssh-connect/run.sh
+++ tests/ssh-connect/run.sh
@@ -11,3 +11,10 @@ CHECKED_TEXT=
 if [[ "1" != `docker-compose exec pgmaster bash -c 'gosu postgres ssh -o LogLevel=QUIET -t pgslave1 "cat /tmp/ssh_check"' | grep SSH_CHECK | wc -l | tr -d ' '` ]]; then
     exit 1
 fi
+
+docker-compose restart pgslave
+sleep 5
+
+if [[ "1" != `docker-compose exec -T pgmaster bash -c 'gosu postgres ssh -o LogLevel=QUIET -t pgslave "cat /tmp/ssh_check"' | grep SSH_CHECK | wc -l | tr -d ' '` ]]; then
+    exit 1
+fi
