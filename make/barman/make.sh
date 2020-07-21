8a018bb86d01cc9267b9fddd62242c49bbd5213b
Marcus Bergner <mbe@vizrt.com>
2020-07-21 12:13:22 +0200

    Use apt-archive.postgresql.org debs and fix dpkg dependency problems
    See also: https://github.com/paunin/PostDock/issues/260
    
    http://apt-archive.postgresql.org/pub/repos/apt/pool/main is now used
    instead of http://atalia.postgresql.org/morgue when referencing old
    Debian packages.
    
    URI escaping of + to %2B in apt-archive urls to ensure wget download
    works as expected from apt-archive.
    
    Fixed dpkg dependency problems for pgpool and libpgpool0 by apt-get
    install-ing the following packages:
    postgresql-common
    
    Fixed dpkg dependency problems for barman by apt-get install-ing the
    following packages:
    python-argcomplete python-argh python-dateutil python-psycopg2 rsync
    
    Fixed dpkg dependency problem for pgpool by ensuring libmemcached11 is
    installed for pgpool 3.6 and 3.7, but pgpool 3.3 gets libmemcached10.

--- make/barman/make.sh
+++ make/barman/make.sh
@@ -1,7 +1,7 @@
 echo ">>> Making barman"
 
-for VALS in "BARMAN_VERSION=2.3 BARMAN_PACKAGE_VERSION=2.3-2.pgdg90+1"\
-            "BARMAN_VERSION=2.4 BARMAN_PACKAGE_VERSION=2.4-1.pgdg90+1"; do
+for VALS in "BARMAN_VERSION=2.3 BARMAN_PACKAGE_VERSION=2.3-2.pgdg%2B1"\
+            "BARMAN_VERSION=2.4 BARMAN_PACKAGE_VERSION=2.4-1.pgdg%2B1"; do
     eval $VALS
 
     for PG_CLIENT_VERSION in 9.6 10 11; do
